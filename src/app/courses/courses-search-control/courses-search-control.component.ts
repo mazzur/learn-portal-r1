e240c71b91cdd20035c5e20e459de71613b28f93
mazzur <mazzur55@gmail.com>
2020-04-22 23:01:56 +0300

    Courses page layout updates

---
+++ src/app/courses/courses-search-control/courses-search-control.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'lp-courses-search-control',
+  templateUrl: './courses-search-control.component.html',
+  styleUrls: ['./courses-search-control.component.scss']
+})
+export class CoursesSearchControlComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
